1678b9d2039da06301656136af7b2655d307f8bf
dinlan94 <fajarudin96@gmail.com>
2023-02-17 21:15:05 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,17 +6,18 @@ import Search from './Search';
 const Banner = () => {
   return (
     <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
-      <div className='flex flex-col lg:flex-row'>
-        <div className='lg:ml-8 xl:ml-[135px] flex-col items-center 
+      <div className=' container mx-auto flex flex-col lg:flex-row mb-10'>
+        <div className='lg:ml-6 xl:ml-[70px] flex-col items-center 
         lg:items-start text-center lg:text-left justify-center
         flex-1 px-4 lg:px-0'>
           <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'>
             <span className='text-violet-700'>Rent</span> Your Dream House With Us.
           </h1>
           <p className='max-w-[480px] mb-8'>
-            lorem ipsum dolor si amet adadadasdsadasd
-            asdasdsadasdaasdadasdsadasdasfafasfsafasfaf
-            afafasfsafasf
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat.
+            Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab
+            distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius
+            dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.
           </p>
         </div>
         {/* image */}
